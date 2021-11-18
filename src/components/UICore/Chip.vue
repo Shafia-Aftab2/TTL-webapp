881f155a6d716169a3468a2c947d34ed5ae21a2b
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-11-18 18:43:53 +0500

    feat: chip component

---
+++ src/components/UICore/Chip.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    type="button"
+    :class="['talkie-chip', customClass.toString()]"
+    @click="onClick"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "TalkieChip",
+  props: {
+    label: {
+      type: String,
+      default: "Chip"
+    },
+    onClick: { type: Function, default: () => {} },
+    customClass: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+.talkie-chip {
+  display: inline-block;
+  color: var(--t-black-100);
+  background: transparent;
+  border-radius: var(--t-br-large);
+  width: fit-content;
+  cursor: pointer;
+  border: transparent;
+  transition: 0.1s ease;
+  text-transform: capitalize;
+}
+.talkie-chip:hover {
+  filter: opacity(0.8);
+}
+</style>
