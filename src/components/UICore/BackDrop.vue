2770dd392181b0a00c88899de7c67c102e233f88
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-11-18 15:00:40 +0500

    feat: backdrop component added

---
+++ src/components/UICore/BackDrop.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="[`talkie-backdrop`, customClass.toString()]"><slot /></div>
+</template>
+
+<script>
+export default {
+  name: "TalkieBackDrop",
+  components: {},
+  props: {
+    customClass: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+.talkie-backdrop {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: var(--t-zindex-10);
+  background-color: rgba(128, 128, 128, 0.5);
+}
+</style>
