a10c8db0028627526b123b4ee9deca7ce20acbe7
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-11-18 19:44:08 +0500

    chore: export all UI elements from single file

---
+++ src/components/UICore/index.js
@@ -0,0 +1,19 @@
+import TalkieBackDrop from "./BackDrop.vue";
+import TalkieLoader from "./Loader.vue";
+import TalkieBackDropLoader from "./BackDropLoader.vue";
+import TalkieButton from "./Button.vue";
+import TalkieInput from "./Input.vue";
+import TalkieSelect from "./Select.vue";
+import TalkieChip from "./Chip.vue";
+import TalkieSwitch from "./Switch.vue";
+
+export {
+  TalkieBackDrop,
+  TalkieLoader,
+  TalkieBackDropLoader,
+  TalkieButton,
+  TalkieInput,
+  TalkieSelect,
+  TalkieChip,
+  TalkieSwitch
+};
