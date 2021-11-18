de247dd2e1a4be98ec2d4305028805003c1b681c
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-11-18 19:02:08 +0500

    feat: switch component added

---
+++ src/components/UICore/Switch.vue
@@ -0,0 +1,98 @@
+<template>
+  <div :class="['talkie-switch-wrapper', customClass.toString()]">
+    <div>
+      <input
+        :id="key"
+        :name="name"
+        :class="['talkie-switch']"
+        type="checkbox"
+        :checked="isChecked"
+        @change="handleSwitchToggle"
+      />
+      <label :for="key"></label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TalkieSwitch",
+  components: {},
+  data() {
+    return {
+      isChecked: false,
+      key: `talkie-switch-${Math.floor(Math.random() * 1234567890)}`
+    };
+  },
+  props: {
+    onToggle: { type: Function, default: () => {} },
+    name: {
+      type: String
+    },
+    customClass: {
+      type: String,
+      default: ""
+    }
+  },
+  methods: {
+    async handleSwitchToggle(e) {
+      const checkStatus = e.target.checked;
+      this.isChecked = checkStatus;
+      if (this.onToggle) await this.onToggle(!!checkStatus);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.talkie-switch-wrapper {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  /* Default variant */
+  --background: var(--t-black-100);
+  --stale: var(--t-white-100);
+  --active: var(--t-green);
+  /* Default Size */
+  --width: var(--t-space-50);
+  --height: var(--t-space-24);
+  --padding: var(--t-space-4);
+  --ball-size: calc(var(--height) - (var(--padding) * 2));
+}
+
+/* Switch Function */
+.talkie-switch {
+  opacity: 0;
+  pointer-events: none;
+  position: absolute;
+}
+.talkie-switch + label {
+  display: inline-flex;
+  align-items: center;
+  width: var(--width);
+  height: var(--height);
+  background-color: var(--background);
+  border-radius: 100px;
+  cursor: pointer;
+  transition: 0.25s ease;
+}
+.talkie-switch + label:after {
+  content: "";
+  width: var(--ball-size);
+  height: var(--ball-size);
+  background-color: var(--stale);
+  border-radius: 50%;
+  margin-left: calc((var(--height) - var(--ball-size)) / 2);
+  transition: 0.25s ease;
+  transform: translateX(0);
+}
+.talkie-switch:checked + label {
+  background-color: var(--active);
+}
+.talkie-switch:checked + label:after {
+  background-color: var(--background);
+  transform: translateX(
+    calc(var(--width) - var(--ball-size) - var(--padding) * 2)
+  );
+}
+</style>
