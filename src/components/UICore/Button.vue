b16f40bdd989b43c19565b2a76ca0068a5032cc8
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-11-18 15:14:22 +0500

    feat: button component added

---
+++ src/components/UICore/Button.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    :type="type"
+    :class="['talkie-button', customClass.toString()]"
+    @click="onClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "TalkieButton",
+  components: {},
+  props: {
+    type: {
+      type: String,
+      default: "submit"
+    },
+    onClick: {
+      type: Function,
+      default: () => {}
+    },
+    customClass: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+.talkie-button {
+  background: transparent;
+  border: transparent;
+  cursor: pointer;
+  position: relative;
+  display: inline-flex;
+  width: fit-content;
+  justify-content: center;
+  align-items: center;
+  transition: 0.1s ease;
+  color: var(--t-black-100);
+  border-radius: var(--t-br-medium);
+  font-family: var(--t-ff-regular);
+  font-size: var(--t-fs-base);
+}
+</style>
