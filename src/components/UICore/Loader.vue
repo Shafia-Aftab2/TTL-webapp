9ebb9bd092b7a77d1d9feffc29214843acdef75f
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-11-20 20:58:16 +0500

    feat: loader component made responsive

--- src/components/UICore/Loader.vue
+++ src/components/UICore/Loader.vue
@@ -31,6 +31,7 @@ export default {
   border-radius: 50%;
   border: var(--t-space-3) solid var(--t-white);
   border-top: var(--t-space-3) solid var(--t-black-50);
+  border-width: var(--border-width);
   /* Animation */
   animation: talkie-spinner 0.5s linear infinite;
   -webkit-animation: talkie-spinner 0.5s linear infinite; /* Animation (for Safari) */
@@ -38,19 +39,16 @@ export default {
 
 /* Size Variants */
 .talkie-loader-small {
-  width: var(--t-space-20);
-  height: var(--t-space-20);
-  border-width: var(--t-space-2);
+  --size: var(--t-space-20);
+  --border-width: var(--t-space-2);
 }
 .talkie-loader-medium {
-  width: var(--t-space-22);
-  height: var(--t-space-22);
-  border-width: var(--t-space-2);
+  --size: var(--t-space-22);
+  --border-width: var(--t-space-2);
 }
 .talkie-loader-large {
-  width: var(--t-space-28);
-  height: var(--t-space-28);
-  border-width: var(--t-space-3);
+  --size: var(--t-space-28);
+  --border-width: var(--t-space-3);
 }
 
 /* Spinner */
@@ -69,6 +67,26 @@ export default {
   }
   100% {
     -webkit-transform: rotate(360deg);
+  }
+}
+
+/* Responsive variants */
+@media (max-width: 599px) {
+  .talkie-loader {
+    width: calc(var(--size) / 1.25);
+    height: calc(var(--size) / 1.25);
+  }
+}
+@media (min-width: 600px) {
+  .talkie-loader {
+    width: calc(var(--size) / 1.15);
+    height: calc(var(--size) / 1.15);
+  }
+}
+@media (min-width: 1200px) {
+  .talkie-loader {
+    width: var(--size);
+    height: var(--size);
   }
 }
 </style>
