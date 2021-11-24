249f5334f96f79a0cf8b92b1915a353a30d270b4
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-11-24 14:59:57 +0500

    feat: teachers signup component made responsive

--- src/components/Modules/Teachers/Signup/Signup.vue
+++ src/components/Modules/Teachers/Signup/Signup.vue
@@ -80,10 +80,6 @@ export default {
   width: 100%;
   margin: auto;
   margin-top: var(--t-space-12);
-  flex-direction: row;
-  gap: var(--t-space-24);
-  padding: var(--t-space-48) 0;
-  max-width: 100%;
 }
 .teachers-signup-avatar {
   display: flex;
@@ -93,14 +89,6 @@ export default {
   --logo-width: 500px;
   --logo-height: 500px;
 }
-.teachers-signup-avatar > svg {
-  position: static;
-  opacity: 1;
-  transform: translate(0, 0);
-  z-index: var(--t-zindex-10);
-  width: var(--logo-width);
-  height: var(--logo-height);
-}
 .teachers-signup-form {
   display: flex;
   flex: 1;
@@ -108,7 +96,6 @@ export default {
   justify-content: center;
   width: 100%;
   height: 100%;
-  gap: var(--t-space-16);
 }
 .teachers-signup-form-options {
   display: flex;
@@ -116,12 +103,10 @@ export default {
   justify-content: center;
   align-items: center;
   padding: var(--t-space-12);
-  gap: var(--t-space-24);
 }
 .teachers-signup-form-options-info {
   padding: var(--t-space-4);
   text-align: center;
-  font-size: var(--t-fs-small);
 }
 .teachers-signup-form-options-info-link {
   text-decoration: none;
@@ -130,5 +115,85 @@ export default {
 }
 .teachers-signup-form-options-info-link:visited {
   color: var(--t-black);
+}
+
+/* Responsive variants */
+@media (max-width: 599px) {
+  .teachers-signup-wrapper {
+    flex-direction: column;
+    gap: var(--t-space-16);
+    padding: var(--t-space-32);
+  }
+  .teachers-signup-avatar {
+    display: none;
+  }
+  .teachers-signup-form {
+    gap: var(--t-space-12);
+  }
+  .teachers-signup-form-options {
+    gap: var(--t-space-12);
+  }
+  .teachers-signup-form-options-info {
+    font-size: calc(var(--t-fs-small) - 2px);
+  }
+}
+@media (min-width: 600px) {
+  .teachers-signup-wrapper {
+    flex-direction: column;
+    gap: var(--t-space-24);
+    padding: var(--t-space-32);
+    max-width: 75%;
+  }
+  .teachers-signup-avatar {
+    display: flex;
+  }
+  .teachers-signup-avatar > svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-150%, -30%);
+    opacity: 0.5;
+    z-index: var(--t-zindex-nv-10);
+    width: calc(var(--logo-width) * 0.9);
+    height: calc(var(--logo-height) * 0.9);
+  }
+  .teachers-signup-form {
+    gap: var(--t-space-12);
+  }
+  .teachers-signup-form-options {
+    gap: var(--t-space-24);
+  }
+  .teachers-signup-form-options-info {
+    font-size: var(--t-fs-small);
+  }
+}
+@media (min-width: 900px) {
+  .teachers-signup-wrapper {
+    flex-direction: row;
+    gap: var(--t-space-16);
+    padding: var(--t-space-58);
+    max-width: 100%;
+  }
+  .teachers-signup-avatar > svg {
+    position: static;
+    opacity: 1;
+    transform: translate(0, 0);
+    z-index: var(--t-zindex-10);
+    width: calc(var(--logo-width) * 0.8);
+    height: calc(var(--logo-height) * 0.8);
+  }
+}
+@media (min-width: 1200px) {
+  .teachers-signup-wrapper {
+    gap: var(--t-space-24);
+    padding: var(--t-space-48) 0;
+  }
+  .teachers-signup-avatar > svg {
+    width: var(--logo-width);
+    height: var(--logo-height);
+  }
+  .teachers-signup-form {
+    gap: var(--t-space-16);
+  }
 }
 </style>
