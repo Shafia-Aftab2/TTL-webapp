b6a39fdece0e5d25139f9b30c1f32c1f02c52c42
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-11-27 15:36:44 +0500

    feat: talkie form validator component

---
+++ src/components/UICore/Form.vue
@@ -0,0 +1,93 @@
+<template>
+  <form @submit="handleSubmit($event, onSubmit)">
+    <slot
+      :errors="errors"
+      :isSubmitting="isSubmitting"
+      :submitCount="submitCount"
+      :state="state"
+      :values="values"
+      :setError="setError"
+      :setErrors="setErrors"
+      :setValue="setValue"
+      :setValues="setValues"
+      :validate="validate"
+      :submitForm="submitForm"
+      :resetForm="resetForm"
+    />
+  </form>
+</template>
+
+<script>
+import { useForm } from "vee-validate";
+
+export default {
+  name: "TalkieForm",
+  data: () => {
+    const {
+      errors,
+      isSubmitting,
+      submitCount,
+      meta: state,
+      values,
+      setFieldError: setError,
+      setErrors,
+      setFieldValue: setValue,
+      setValues,
+      validate,
+      handleSubmit,
+      submitForm,
+      handleReset,
+      resetForm,
+    } = useForm({
+      validationSchema: this.validationSchema,
+      initialErrors: this.initialErrors,
+      initialValues: this.initialValues,
+      initialTouched: this.initialTouched,
+      validateOnMount: this.validateOnMount,
+    });
+
+    return {
+      errors,
+      isSubmitting,
+      submitCount,
+      state,
+      values,
+      setError,
+      setErrors,
+      setValue,
+      setValues,
+      validate,
+      handleSubmit,
+      submitForm,
+      handleReset,
+      resetForm,
+    };
+  },
+  props: {
+    validationSchema: {
+      type: Object,
+      default: () => ({}),
+    },
+    initialErrors: {
+      type: Object,
+      default: () => ({}),
+    },
+    initialValues: {
+      type: Object,
+      default: () => ({}),
+    },
+    initialTouched: {
+      type: Object,
+      default: () => ({}),
+    },
+    validateOnMount: {
+      type: Boolean,
+      default: false,
+    },
+    onSubmit: {
+      type: Function,
+      default: () => {},
+    },
+  },
+};
+</script>
