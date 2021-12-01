2bd4ed95a8c7ca1f68cedcfbffe0852e31e4df29
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-01 19:40:25 +0500

    feat: audio recorder sub module

---
+++ src/components/SubModules/AudioManager/AudioRecorder.vue
@@ -0,0 +1,91 @@
+<template>
+  <slot
+    :toggleRecording="toggleRecording"
+    :startRecording="startRecording"
+    :stopRecording="stopRecording"
+    :pauseRecording="pauseRecording"
+    :isRecording="isRecording"
+    :isPaused="isPaused"
+    :recordingDuration="recordingDuration"
+  />
+</template>
+
+<script>
+import Recorder from "./lib/recorder";
+import { convertTimeMMSS } from "./lib/utils";
+
+export default {
+  data() {
+    return {
+      recorder: this._initRecorder(),
+      recordList: [],
+    };
+  },
+  props: {
+    audioFormat: { type: String, default: "mp3" },
+    audioTimeLimitInMinutes: { type: Number },
+    audioSampleBitRate: { type: Number, default: 128 },
+    audioSampleRate: { type: Number, default: 44100 },
+    onRecordingStart: { type: Function },
+    onRecordingPaused: { type: Function },
+    onRecordingStopped: { type: Function },
+    onMikeFailure: { type: Function },
+  },
+  beforeUnmount() {
+    this.stopRecorder();
+  },
+  methods: {
+    _initRecorder() {
+      return new Recorder({
+        onRecordingStart: this.onRecordingStart,
+        onRecordingStopped: this.onRecordingStopped,
+        onRecordingPaused: this.onRecordingPaused,
+        micFailed: this.onMikeFailure,
+        bitRate: this.audioSampleBitRate,
+        sampleRate: this.audioSampleRate,
+        format: this.audioFormat,
+      });
+    },
+    toggleRecording() {
+      if (!this.isRecording || (this.isRecording && this.isPaused)) {
+        this.recorder.start();
+      } else {
+        this.recorder.pause();
+      }
+    },
+    stopRecording() {
+      if (!this.isRecording) return;
+      this.recorder.stop();
+      this.recordList = this.recorder.recordList();
+    },
+    startRecording() {
+      if (!this.isRecording || (this.isRecording && this.isPaused)) {
+        this.recordList = [];
+        this.recorder.start();
+      }
+    },
+    pauseRecording() {
+      if (!(!this.isRecording || (this.isRecording && this.isPaused))) {
+        this.recorder.pause();
+      }
+    },
+  },
+  computed: {
+    isPaused() {
+      return this.recorder.isPause;
+    },
+    isRecording() {
+      return this.recorder.isRecording;
+    },
+    recordingDuration() {
+      if (
+        this.audioTimeLimitInMinutes &&
+        this.recorder.duration >= this.audioTimeLimitInMinutes * 60
+      ) {
+        this.stopRecorder();
+      }
+      return convertTimeMMSS(this.recorder.duration);
+    },
+  },
+};
+</script>
