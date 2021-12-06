b1b84081973b22103ad161bd84490f0ca4ae7ab4
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-06 15:35:21 +0500

    feat: common layout added (layout entry point)

---
+++ src/components/Layouts/_layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <talkie-header />
+  <wrapper-layout v-if="type === 'default'" />
+  <sidebar-layout v-if="type === 'sidebar'" />
+</template>
+
+<script>
+import TalkieHeader from "./Header.vue";
+import WrapperLayout from "./WrapperLayout.vue";
+import SidebarLayout from "./SidebarLayout.vue";
+
+export default {
+  name: "Layout",
+  components: {
+    TalkieHeader,
+    WrapperLayout,
+    SidebarLayout,
+  },
+  props: {
+    type: {
+      type: String,
+      default: "default",
+      validator: (val) => ["default", "sidebar"].includes(val),
+    },
+  },
+};
+</script>
