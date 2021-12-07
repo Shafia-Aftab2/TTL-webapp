dde8097a515bdaf65f0e765afc005044127c4d56
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-07 22:07:10 +0500

    feat: question card comp added

---
+++ src/components/SubModules/Cards/Question/Question.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="talkie-question-card-wrapper">
+    <div class="talkie-question-card-details-wrapper">
+      <img class="talkie-question-card-image" :src="image" v-if="image" />
+      <div class="talkie-question-card-details-header-wrapper">
+        <h5 class="h5" v-if="title">{{ title }}</h5>
+        <p class="p" v-if="topic">Topic: {{ topic }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestionCard",
+  props: {
+    title: {
+      type: String,
+    },
+    topic: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.talkie-question-card-wrapper {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--t-white);
+  width: 100%;
+  border-radius: var(--t-br-large);
+  padding: var(--t-space-16);
+  gap: var(--t-space-16);
+}
+.talkie-question-card-image {
+  border-radius: 50%;
+  object-fit: cover;
+  min-height: var(--t-space-70);
+  min-width: var(--t-space-70);
+  height: var(--t-space-70);
+  width: var(--t-space-70);
+  box-shadow: inset 0 0 0 calc(var(--t-space-70) / 10) var(--t-secondary);
+}
+.talkie-question-card-details-wrapper {
+  display: flex;
+  align-items: center;
+  gap: var(--t-space-16);
+}
+.talkie-question-card-details-header-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: var(--t-space-16);
+}
+
+/* TEMP */
+.p {
+  margin-bottom: 0 !important;
+}
+</style>
---
+++ src/components/SubModules/Cards/Question/index.js
@@ -0,0 +1,2 @@
+import Question from "./Question.vue";
+export default Question;
