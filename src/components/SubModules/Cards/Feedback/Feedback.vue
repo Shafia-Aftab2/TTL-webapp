cb0e9d29a768fddbc6c3128bef001e66c17577a1
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-09 14:43:49 +0500

    feat: audio recorder/player added, made responsive - feedback card

--- src/components/SubModules/Cards/Feedback/Feedback.vue
+++ src/components/SubModules/Cards/Feedback/Feedback.vue
@@ -9,33 +9,108 @@
       <talkie-input :placeholder="inputPlaceholder" />
     </div>
 
+    <!-- Recorder -->
     <div class="talkie-feedback-card-options">
-      <talkie-icon
-        :name="'mike-unmuted'"
-        :isActive="true"
-        :variant="'secondary'"
-        :size="40"
-        :iconToSizeRatio="1.1"
-      />
-      <talkie-icon
-        :name="'play'"
-        :isActive="true"
-        :variant="'primary'"
-        :size="40"
-        :iconToSizeRatio="1.1"
-      />
+      <talkie-audio-recorder
+        v-slot="{ startRecording, stopRecording, isRecording }"
+        :onRecordingStopped="handleRecordedItem"
+        v-if="!currentRecording"
+      >
+        <talkie-icon
+          :name="'mike-unmuted'"
+          :isActive="true"
+          :variant="'secondary'"
+          :size="40"
+          :iconToSizeRatio="1.1"
+          :onClick="startRecording"
+          v-if="!isRecording"
+        />
+        <talkie-icon
+          :name="'square'"
+          :isActive="true"
+          :variant="'secondary'"
+          :size="40"
+          :iconToSizeRatio="1.5"
+          :customClass="'talkie-feedback-card-stop-recording-button'"
+          :onClick="stopRecording"
+          v-if="isRecording"
+        />
+      </talkie-audio-recorder>
+      <!-- Player -->
+      <talkie-audio-player
+        v-slot="{
+          isPlaying,
+          togglePlayer,
+          totalAudioPlaybackTime,
+          currentAudioPlaybackTime,
+        }"
+        :recording="currentRecording"
+        v-if="currentRecording"
+      >
+        <span class="talkie-feedback-card-options-audio-timestamps">
+          {{ currentAudioPlaybackTime }} /
+          {{ totalAudioPlaybackTime }}
+        </span>
+        <span class="talkie-feedback-card-options-audio-player-icons">
+          <talkie-icon
+            :name="'play'"
+            :isActive="true"
+            :variant="'primary'"
+            :size="40"
+            :iconToSizeRatio="1.1"
+            :onClick="togglePlayer"
+            v-if="!isPlaying"
+          />
+          <talkie-icon
+            :name="'pause'"
+            :isActive="true"
+            :variant="'primary'"
+            :size="40"
+            :iconToSizeRatio="1.1"
+            :onClick="togglePlayer"
+            v-if="isPlaying"
+          />
+          <talkie-icon
+            :name="'arrow-rounded-left'"
+            :isActive="true"
+            :variant="'secondary'"
+            :size="40"
+            :iconToSizeRatio="1.1"
+            :onClick="handleRecordingReset"
+          />
+          <talkie-icon
+            :name="'send'"
+            :isActive="true"
+            :variant="'secondary'"
+            :size="40"
+            :iconToSizeRatio="1.1"
+            :onClick="onFeedbackSendClick"
+          />
+        </span>
+      </talkie-audio-player>
     </div>
   </div>
 </template>
 
 <script>
 import { TalkieIcon, TalkieInput } from "@/components/UICore";
+import {
+  TalkieAudioRecorder,
+  TalkieAudioPlayer,
+} from "@/components/SubModules/AudioManager";
 
 export default {
   name: "FeedbackCard",
+  data() {
+    return {
+      currentRecording: null,
+    };
+  },
   components: {
     TalkieIcon,
     TalkieInput,
+    TalkieAudioRecorder,
+    TalkieAudioPlayer,
   },
   props: {
     inputPlaceholder: {
@@ -45,6 +120,28 @@ export default {
     fullWidth: {
       type: Boolean,
       default: true,
+    },
+    onFeedbackRecording: {
+      type: Function,
+      default: () => {},
+    },
+    onFeedbackRecordingDiscard: {
+      type: Function,
+      default: () => {},
+    },
+    onFeedbackSendClick: {
+      type: Function,
+      default: () => {},
+    },
+  },
+  methods: {
+    handleRecordedItem(recording) {
+      this.currentRecording = recording;
+      this.onFeedbackRecording && this.onFeedbackRecording(recording);
+    },
+    handleRecordingReset() {
+      this.currentRecording = null;
+      this.onFeedbackRecordingDiscard && this.onFeedbackRecordingDiscard();
     },
   },
 };
@@ -74,6 +171,24 @@ export default {
   background: transparent !important;
 }
 .talkie-feedback-card-options {
+  display: flex;
+  align-items: center;
+  gap: var(--t-space-12);
+}
+.talkie-feedback-card-stop-recording-button {
+  border-color: var(--t-secondary) !important;
+  border-style: solid !important;
+  border-width: var(--t-space-3);
+}
+.talkie-feedback-card-options-audio-timestamps {
+  background: var(--t-primary);
+  padding: var(--t-space-3) var(--t-space-5);
+  border-radius: var(--t-br-medium);
+  text-align: center;
+  white-space: nowrap;
+  font-size: var(--t-fs-small);
+}
+.talkie-feedback-card-options-audio-player-icons {
   display: flex;
   align-items: center;
   gap: var(--t-space-12);
