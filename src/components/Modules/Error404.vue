2943e8de99a9c62af93b37c4aed9925204f01ea5
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-11 01:18:49 +0500

    feat: 404 oage added

---
+++ src/components/Modules/Error404.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="error-404-wrapper">
+    <h5 class="h5">404 Not Found</h5>
+    <talkie-button :size="'medium'" :noHighlights="true">
+      Go Back
+    </talkie-button>
+  </div>
+</template>
+
+<script>
+import { TalkieButton } from "@/components/UICore";
+
+export default {
+  name: "Error404",
+  components: {
+    TalkieButton,
+  },
+};
+</script>
+
+<style scoped>
+.error-404-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+  gap: var(--t-space-24);
+  flex-direction: row;
+}
+</style>
