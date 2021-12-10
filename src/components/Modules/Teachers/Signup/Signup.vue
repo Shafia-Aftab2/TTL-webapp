a8ded7132e70a5242486a157949071a4e524d7d0
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-10 18:19:00 +0500

    refactor: api mock removed - teachers signup api

--- src/components/Modules/Teachers/Signup/Signup.vue
+++ src/components/Modules/Teachers/Signup/Signup.vue
@@ -10,7 +10,7 @@
       :customClass="'teachers-signup-form'"
       v-slot="{ errors }"
       :validationSchema="teacherSignupSchema"
-      :onSubmit="mockAPI"
+      :onSubmit="handleSubmit"
     >
       <h3 class="h3">Create A Teacher Account</h3>
       <talkie-input
@@ -163,22 +163,6 @@ export default {
         type: "success",
         message: "Account Created. Redirecting..!",
       };
-    },
-    async mockAPI(values) {
-      this.loading = true;
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-      this.loading = false;
-      if (values.email === "success@talkie.com") {
-        this.formStatus = {
-          type: "success",
-          message: "Account created. Redirecting...!",
-        };
-      } else {
-        this.formStatus = {
-          type: "error",
-          message: "Could not create account.",
-        };
-      }
     },
   },
 };
