6254aea03e9f5be73cfad5dbaa4129370296d5b1
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-14 01:24:50 +0500

    feat: class join api connected - state indicators added - student join class

--- src/components/Modules/Class/Join/Join.vue
+++ src/components/Modules/Class/Join/Join.vue
@@ -1,23 +1,81 @@
 <template>
   <div class="teachers-class-join-wrapper">
-    <div class="teachers-class-join-content-wrapper">
-      <h3 class="h3">
-        You are now a member of
-        <a class="teachers-class-join-content-class-name">Spanish 10A</a>
-      </h3>
+    <!-- Contents -->
+    <template v-if="!loading">
+      <div class="teachers-class-join-content-wrapper">
+        <h3 class="h3" v-if="isJoined">
+          You are now a member of
+          <a class="teachers-class-join-content-class-name">
+            {{ classDetails.name }}
+          </a>
+        </h3>
+        <h3 class="h3" v-if="!isJoined">Could not join class..!</h3>
 
-      <talkie-button>Go To Class</talkie-button>
-    </div>
+        <talkie-button>Go To Class</talkie-button>
+      </div>
+    </template>
+
+    <!-- Load wrapper -->
+    <template v-if="loading">
+      <div class="teachers-class-join-loading-wrapper">
+        <talkie-loader :size="'large'" />
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
-import { TalkieButton } from "@/components/UICore";
+import { TalkieLoader, TalkieButton } from "@/components/UICore";
+import { ClassService } from "@/api/services";
 
 export default {
   name: "ClassJoin",
   components: {
+    TalkieLoader,
     TalkieButton,
+  },
+  data() {
+    return {
+      loading: false,
+      isJoined: false,
+      classId: null,
+      classDetails: {},
+    };
+  },
+  async created() {
+    // get class id from params
+    const classId = this.$route.params.id;
+    this.classId = classId;
+
+    // init join class sequence
+    await this.handleClassJoinSequence();
+  },
+  methods: {
+    async handleClassJoinSequence() {
+      // update page state
+      this.loading = true;
+
+      // api call
+      const response = await ClassService.JoinAsStudent(this.classId).catch(
+        () => null
+      );
+
+      // failure case
+      if (!response) {
+        this.isJoined = false;
+        this.loading = false;
+        return;
+      }
+
+      // success case
+      const classDetails = response.data;
+      this.classDetails = {
+        id: classDetails.id,
+        name: classDetails.name,
+      };
+      this.isJoined = true;
+      this.loading = false;
+    },
   },
 };
 </script>
