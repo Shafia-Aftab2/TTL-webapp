80b02266ff7dde8556b7bd0b7351e163a0fd557a
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-13 16:08:56 +0500

    chore: file api services added - upload file service added

--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,4 +1,6 @@
 import AuthService from "./Auth.service";
 import ClassService from "./Class.service";
+import FileService from "./File.service";
+import TaskService from "./Task.service";
 
-export { AuthService, ClassService };
+export { AuthService, ClassService, FileService, TaskService };
