bac454494fa963f89014a54d80fe1d6790267047
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-14 17:26:28 +0500

    chore: class details api service added

--- src/api/services/Class.service.js
+++ src/api/services/Class.service.js
@@ -5,6 +5,10 @@ export default class ClassRoutes {
     return HTTPClient.post(`/class`, payload);
   }
 
+  static async GetDetails(id) {
+    return HTTPClient.get(`/class/${id}/details`);
+  }
+
   static async AddTopics(id, payload) {
     return HTTPClient.post(`/class/${id}/topics`, payload);
   }
