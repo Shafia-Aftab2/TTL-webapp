832e2a51b35ab290ab14ae5add3e5773ed9531b4
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-14 17:42:16 +0500

    chore: query class tasks api service added - tasks

--- src/api/services/Task.service.js
+++ src/api/services/Task.service.js
@@ -4,4 +4,12 @@ export default class TaskRoutes {
   static async Create(classId, payload) {
     return HTTPClient.post(`/tasks/${classId}`, payload);
   }
+
+  static async QueryClassTasks(classId, query) {
+    return HTTPClient.get(
+      `/tasks/${classId}?${Object.entries(query).map(
+        ([key, value]) => `${key}=${encodeURIComponent(value)}&`
+      )}`
+    );
+  }
 }
