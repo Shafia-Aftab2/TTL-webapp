181ac04c17491e48209c0b7b2ab56d96e70420ef
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-15 19:35:55 +0500

    chore: task details api service added - tasks

--- src/api/services/Task.service.js
+++ src/api/services/Task.service.js
@@ -5,6 +5,10 @@ export default class TaskRoutes {
     return HTTPClient.post(`/tasks/${classId}`, payload);
   }
 
+  static async GetDetails(taskId) {
+    return HTTPClient.get(`/tasks/${taskId}/details`);
+  }
+
   static async QueryClassTasks(classId, query) {
     return HTTPClient.get(
       `/tasks/${classId}?${Object.entries(query).map(
