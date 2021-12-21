e5d79d8c7dd64ae4564f0fbd9612305c3ee63ebb
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-22 01:31:13 +0500

    refactor: join module moved to join/link - class join

---
+++ src/components/Modules/Class/Join/Link/Link.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="class-join-link-wrapper">
+    <!-- Contents -->
+    <template v-if="!loading">
+      <div class="class-join-link-content-wrapper">
+        <h3 class="h3" v-if="isJoined">
+          You are now a member of
+          <a
+            class="class-join-link-content-class-name"
+            :href="classDetails.link"
+          >
+            {{ classDetails.name }}
+          </a>
+        </h3>
+        <h3 class="h3" v-if="!isJoined">Could not join class..!</h3>
+
+        <talkie-button :onClick="handleRedirection">
+          {{ isJoined ? `Go To Class` : `Try Again` }}
+        </talkie-button>
+      </div>
+    </template>
+
+    <!-- Load wrapper -->
+    <template v-if="loading">
+      <div class="class-join-link-loading-wrapper">
+        <talkie-loader :size="'large'" />
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import { TalkieLoader, TalkieButton } from "@/components/UICore";
+import { ClassService } from "@/api/services";
+
+export default {
+  name: "ClassJoinLink",
+  components: {
+    TalkieLoader,
+    TalkieButton,
+  },
+  data() {
+    return {
+      loading: false,
+      isJoined: false,
+      classId: null,
+      classDetails: {},
+    };
+  },
+  async created() {
+    // get class id from params
+    const classId = this.$route.params.id;
+    this.classId = classId;
+
+    // init join class sequence
+    await this.handleClassJoinSequence();
+  },
+  methods: {
+    handleRedirection() {
+      this.$router.push(
+        `/classes/${this.classId}${this.isJoined ? "" : "/join"}`
+      );
+    },
+    async handleClassJoinSequence() {
+      // update page state
+      this.loading = true;
+
+      // api call
+      const response = await ClassService.JoinAsStudent(this.classId).catch(
+        () => null
+      );
+
+      // failure case
+      if (!response) {
+        this.isJoined = false;
+        this.loading = false;
+        return;
+      }
+
+      // success case
+      const classDetails = response.data;
+      this.classDetails = {
+        id: classDetails.id,
+        name: classDetails.name,
+        link: `/classes/${classDetails.id}`,
+      };
+      this.isJoined = true;
+      this.loading = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.class-join-link-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  width: 100%;
+  padding: var(--t-space-24);
+  transition: 0.2s ease;
+  min-height: 80vh;
+}
+.class-join-link-content-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+}
+.class-join-link-content-class-name {
+  color: var(--t-secondary);
+}
+.class-join-link-loading-wrapper {
+  margin: auto;
+}
+
+/* Responsive variants */
+@media (max-width: 599px) {
+  .class-join-link-wrapper {
+    gap: var(--t-space-18);
+  }
+  .class-join-link-content-wrapper {
+    gap: var(--t-space-16);
+  }
+}
+@media (min-width: 600px) {
+  .class-join-link-wrapper {
+    gap: var(--t-space-24);
+  }
+  .class-join-link-content-wrapper {
+    gap: var(--t-space-16);
+  }
+}
+@media (min-width: 900px) {
+  .class-join-link-wrapper {
+    gap: var(--t-space-36);
+  }
+}
+
+/* TEMP */
+.p {
+  margin-bottom: 0 !important;
+}
+</style>
