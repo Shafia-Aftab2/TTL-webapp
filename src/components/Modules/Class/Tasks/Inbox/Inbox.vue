34a3c81ff20c19085cbb2ad7b18aece4e7c217c9
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-24 01:01:31 +0500

    chore: student inbox ui added in tasks inbox - class tasks inbox

---
+++ src/components/Modules/Class/Tasks/Inbox/Inbox.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="bar-content">
+    <a class="back"><span>&#8592; </span>back</a>
+    <h1>Inbox</h1>
+    <div>
+      <talkie-select placeholder="Filter by Question"></talkie-select>
+    </div>
+    <div class="bar">
+      <p>Favourite App</p>
+    </div>
+    <div class="talkie-icon-bar">
+      <div class="messege-bar">
+        <p>Audios</p>
+      </div>
+      <div class="bar-icons">
+        <talkie-icon
+          :name="'arrow-rounded-left'"
+          :isActive="true"
+          :variant="'secondary'"
+          :size="30"
+        />
+        <talkie-icon
+          :name="'mike-unmuted'"
+          :isActive="true"
+          :variant="'secondary'"
+          :size="50"
+        />
+        <talkie-icon
+          :name="'send'"
+          :isActive="true"
+          :variant="'secondary'"
+          :size="30"
+        />
+      </div>
+    </div>
+    <div class="bar-bottom bar-inline">
+      <p>Least Favourite Subject</p>
+      <talkie-chip label=""></talkie-chip>
+    </div>
+  </div>
+</template>
+<script>
+import { TalkieIcon, TalkieSelect, TalkieChip } from "@/components/UICore";
+
+export default {
+  name: "StudentInbox",
+  components: { TalkieIcon, TalkieSelect, TalkieChip },
+};
+</script>
+<style scoped>
+.talkie-icon-bar {
+  position: relative;
+  background-color: var(--t-white);
+  padding: var(--t-space-24);
+  border-radius: var(--t-space-10);
+}
+.bar-icons {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  bottom: -50px;
+  left: 35%;
+}
+.bar-icons-pg {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  left: 30%;
+}
+
+.bar-bottom {
+  margin-top: 60px;
+  background-color: var(--t-white);
+  padding: var(--t-space-24);
+  border-radius: var(--t-space-10);
+}
+.messege-bar {
+  margin-bottom: 20px;
+}
+.bar-content {
+  display: grid;
+  gap: 10px;
+  padding: var(--t-space-18);
+}
+.bar {
+  background-color: var(--t-white);
+  padding: var(--t-space-24);
+  border-radius: var(--t-space-10);
+}
+.bar-outline {
+  background-color: var(--t-white);
+  padding: var(--t-space-16);
+  border-radius: var(--t-space-10);
+  border: 2px solid var(--t-gray-home);
+}
+.bar-inline {
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+}
+.back {
+  text-decoration: none;
+}
+@media (max-width: 599px) {
+}
+@media (min-width: 600px) {
+}
+@media (min-width: 900px) {
+}
+@media (min-width: 1200px) {
+  .back {
+    position: absolute;
+    left: var(--t-space-50);
+  }
+}
+</style>
