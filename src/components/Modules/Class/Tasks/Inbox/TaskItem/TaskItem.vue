1fe4a4eede2e3e821a9e0442a237de616fc01ce4
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-24 01:42:33 +0500

    feat: student inbox task item ui added - class tasks inbox

---
+++ src/components/Modules/Class/Tasks/Inbox/TaskItem/TaskItem.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="class-tasks-inbox-task-item">
+    <div class="class-tasks-inbox-task-item-header-wrapper">
+      <div class="class-tasks-inbox-task-item-header">
+        <p class="p" v-if="title">{{ title }}</p>
+        <p class="p" v-if="topic">{{ topic }}</p>
+      </div>
+      <div
+        class="class-tasks-inbox-task-item-header-status"
+        v-if="!isRead"
+      ></div>
+    </div>
+    <div class="class-tasks-inbox-task-item-audio-responses-wrapper"></div>
+  </div>
+</template>
+
+<script>
+import { TalkieLoader, TalkieAlert } from "@/components/UICore";
+
+export default {
+  name: "TasksInboxTaskItem",
+  components: {
+    TalkieLoader,
+    TalkieAlert,
+  },
+  props: {
+    title: {
+      type: String,
+    },
+    topic: {
+      type: String,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style>
+.class-tasks-inbox-task-item {
+  display: flex;
+  flex-direction: column;
+  background: var(--t-white);
+  cursor: pointer;
+  gap: var(--t-space-16);
+  border-radius: var(--t-br-large);
+  padding: var(--t-space-24);
+}
+.class-tasks-inbox-task-item-header-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.class-tasks-inbox-task-item-spacer {
+  height: var(--t-space-2);
+  width: 100%;
+  background: var(--t-gray-75);
+}
+.class-tasks-inbox-task-item-header {
+  display: flex;
+  flex-direction: column;
+  gap: var(--t-space-8);
+}
+.class-tasks-inbox-task-item-header-status {
+  display: block;
+  border-radius: 50%;
+  background: var(--t-primary);
+  width: var(--t-space-16);
+  height: var(--t-space-16);
+}
+.class-tasks-inbox-task-item-audio-responses-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: var(--t-space-12);
+  padding: var(--t-space-12) 0;
+}
+.class-tasks-inbox-task-item-audio-message-centered {
+  margin: auto;
+}
+.class-tasks-inbox-task-item-audio-message-right {
+  margin-left: auto;
+}
+</style>
