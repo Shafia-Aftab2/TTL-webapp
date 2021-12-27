90f43f7e2e0524905cbd188f20832a3d87ae7013
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-27 17:48:44 +0500

    chore: query feedbacks api service added - api

--- src/api/services/Feedback.service.js
+++ src/api/services/Feedback.service.js
@@ -1,6 +1,14 @@
 import HTTPClient from "../HTTPClient";
 
 export default class FeedbackRoutes {
+  static async QueryClassTaskFeedbacks(taskId, query) {
+    return HTTPClient.get(
+      `/feedbacks/${taskId}?${Object.entries(query).map(
+        ([key, value]) => `${key}=${encodeURIComponent(value)}&`
+      )}`
+    );
+  }
+
   static async CreateIndividualFeedback(responseId, payload) {
     return HTTPClient.post(`/feedbacks/individual/${responseId}`, payload);
   }
