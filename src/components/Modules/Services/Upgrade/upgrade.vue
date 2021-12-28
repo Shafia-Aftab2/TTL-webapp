e715dcbf94696c42b813994c8f89787f064625d7
muaazseedat <90404874+muaazseedat@users.noreply.github.com>
2021-12-28 13:19:23 +0000

    feat: add back button

--- src/components/Modules/Services/Upgrade/upgrade.vue
+++ src/components/Modules/Services/Upgrade/upgrade.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="upgrade-wrapper">
-    </div>
+  <div class="bar-content">
+    <a class="back"><span>&#8592; </span>back</a>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ServicesUpgrade",
 };
-</script>;
+</script>
