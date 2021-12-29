53222d784bb3e5b04016c7a2edbf13c762bff8f5
muaazseedat <90404874+muaazseedat@users.noreply.github.com>
2021-12-29 10:03:22 +0000

    chore: back button placement

--- src/components/Modules/Services/Upgrade/upgrade.vue
+++ src/components/Modules/Services/Upgrade/upgrade.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="bar-content">
-    <a class="back"><span>&#8592; </span>back</a>
+  <div class="box-container">
+    <div>
+      <div class="back-default">
+        <a class="back"><span>&#8592; </span>back</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +13,70 @@ export default {
   name: "ServicesUpgrade",
 };
 </script>
+<style scoped>
+.bar-outline {
+  background-color: var(--t-white);
+  padding: var(--t-space-16);
+  border-radius: var(--t-space-10);
+  border: 2px solid var(--t-gray-home);
+}
+.bar-inline {
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+}
+.box-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 100px 0 var(--t-space-50) 0;
+  gap: 10px;
+}
+.box-content {
+  display: grid;
+  width: 70%;
+  background-color: white;
+  border-radius: var(--t-br-medium);
+  padding: var(--t-space-20);
+  gap: 10px;
+}
+.text-center {
+  text-align: center;
+}
+.box-header {
+  display: grid;
+  gap: 10px;
+}
+.box-head {
+  display: flex;
+  justify-content: space-between;
+}
+.back {
+  text-decoration: none;
+}
+@media (max-width: 599px) {
+  .box-content {
+    width: 100%;
+  }
+  .box-container {
+    padding: 110px 15px var(--t-space-50) 10px;
+  }
+}
+@media (min-width: 600px) {
+  .back-default {
+    position: absolute;
+    left: 160px;
+  }
+}
+@media (min-width: 900px) {
+}
+@media (min-width: 1200px) {
+  .back-default {
+    position: absolute;
+    left: 160px;
+  }
+}
+</style>
