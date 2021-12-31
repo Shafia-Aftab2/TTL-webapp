436aebc8c9010b2a2f72e7128ac310eebf5f38af
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-31 16:24:01 +0500

    chore: forgot password api service added - auth api

--- src/api/services/Auth.service.js
+++ src/api/services/Auth.service.js
@@ -16,4 +16,8 @@ export default class AuthRoutes {
   static async RefreshTokens(payload) {
     return HTTPClient.post(`/auth/refresh-tokens`, payload);
   }
+
+  static async ForgotPassword(payload) {
+    return HTTPClient.post(`/auth/forgot-password`, payload);
+  }
 }
