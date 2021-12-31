0acb4b1097045291087e77c2012e5e4de1f32932
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-31 18:13:12 +0500

    feat: reset password ui added - auth reset password

---
+++ src/components/Modules/Auth/ResetPassword/ResetPassword.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="auth-reset-password-wrapper">
+    <div class="auth-reset-password-info-wrapper">
+      <h2 class="h2">Reset your password?</h2>
+      <p class="p" style="margin-bottom: 0 !important">
+        Please enter your new password below
+      </p>
+    </div>
+
+    <div class="auth-reset-password-fields-wrapper">
+      <talkie-input
+        :type="'password'"
+        :placeholder="'Password'"
+        :customClass="'auth-reset-password-input'"
+      />
+
+      <talkie-input
+        :type="'password'"
+        :placeholder="'Confirm Password'"
+        :customClass="'auth-reset-password-input'"
+      />
+    </div>
+
+    <div class="auth-reset-password-options-wrapper">
+      <talkie-button :size="'medium'" :type="'submit'">
+        Reset Password
+      </talkie-button>
+    </div>
+
+    <div class="auth-reset-password-footer">
+      <a class="auth-reset-password-footer-link"> Request Password Change </a>
+    </div>
+  </div>
+</template>
+<script>
+import { TalkieInput, TalkieButton } from "@/components/UICore";
+
+export default {
+  name: "AuthResetPassword",
+  components: {
+    TalkieInput,
+    TalkieButton,
+  },
+};
+</script>
+
+<style scoped>
+.auth-reset-password-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  background-color: var(--t-white);
+  gap: var(--t-space-48);
+  margin-top: var(--t-space-70);
+  max-width: 80%;
+  padding: var(--t-space-48);
+  border-radius: var(--t-br-large);
+}
+.auth-reset-password-info-wrapper,
+.auth-reset-password-options-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  gap: var(--t-space-12);
+  width: 100%;
+}
+.auth-reset-password-fields-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  gap: var(--t-space-12);
+  width: 80%;
+}
+.auth-reset-password-input {
+  margin: auto;
+  max-width: 100%;
+}
+.auth-reset-password-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  margin-top: var(--t-space-24);
+  padding: var(--t-space-36);
+}
+.auth-reset-password-footer-link {
+  text-decoration: underline;
+  font-size: var(--t-fs-small);
+}
+.auth-reset-password-footer-link,
+.auth-reset-password-footer-link:hover,
+.auth-reset-password-footer-link:visited {
+  text-decoration: underline;
+  color: var(--t-black);
+}
+</style>
