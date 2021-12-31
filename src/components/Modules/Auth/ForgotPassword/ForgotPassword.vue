ba8af5b2bec18a02301dc203959986e07480e68b
huzaifa-99 <huzaifarasheed900@gmail.com>
2021-12-31 16:30:29 +0500

    feat: forgot password ui made responsive - auth forgot password

--- src/components/Modules/Auth/ForgotPassword/ForgotPassword.vue
+++ src/components/Modules/Auth/ForgotPassword/ForgotPassword.vue
@@ -45,11 +45,6 @@ export default {
   flex-direction: column;
   margin: auto;
   background-color: var(--t-white);
-  max-width: 80%;
-  padding: var(--t-space-48);
-  border-radius: var(--t-br-large);
-  gap: var(--t-space-48);
-  margin-top: var(--t-space-70);
 }
 .auth-forgot-password-info-wrapper,
 .auth-forgot-password-options-wrapper {
@@ -66,28 +61,81 @@ export default {
   justify-content: center;
   margin: auto;
   gap: var(--t-space-12);
-  width: 80%;
 }
 .auth-forgot-password-input {
   margin: auto;
-  max-width: 100%;
 }
 .auth-forgot-password-footer {
   display: flex;
   justify-content: center;
   align-items: center;
   margin: auto;
-  margin-top: var(--t-space-24);
-  padding: var(--t-space-36);
 }
 .auth-forgot-password-footer-link {
   text-decoration: underline;
-  font-size: var(--t-fs-small);
 }
 .auth-forgot-password-footer-link,
 .auth-forgot-password-footer-link:hover,
 .auth-forgot-password-footer-link:visited {
   text-decoration: underline;
   color: var(--t-black);
+}
+
+/* Responsive variants */
+@media (max-width: 599px) {
+  .auth-forgot-password-wrapper {
+    max-width: 100%;
+    gap: var(--t-space-36);
+    padding: var(--t-space-32);
+    margin-top: var(--t-space-50);
+    border-radius: var(--t-br-medium);
+  }
+  .auth-forgot-password-fields-wrapper {
+    width: 100%;
+  }
+  .auth-forgot-password-input {
+    max-width: 100%;
+  }
+  .auth-forgot-password-footer {
+    padding: var(--t-space-50);
+  }
+  .auth-forgot-password-footer-link {
+    font-size: calc(var(--t-fs-small) * 0.9);
+  }
+}
+@media (min-width: 600px) {
+  .auth-forgot-password-wrapper {
+    max-width: 65%;
+    gap: var(--t-space-48);
+    padding: var(--t-space-48);
+    margin-top: var(--t-space-70);
+    border-radius: var(--t-br-medium);
+  }
+  .auth-forgot-password-fields-wrapper {
+    width: 80%;
+  }
+  .auth-forgot-password-input {
+    max-width: 100%;
+  }
+  .auth-forgot-password-footer {
+    margin-top: var(--t-space-24);
+    padding: var(--t-space-36);
+  }
+  .auth-forgot-password-footer-link {
+    font-size: calc(var(--t-fs-small) * 0.9);
+  }
+}
+@media (min-width: 1200px) {
+  .auth-forgot-password-wrapper {
+    max-width: 80%;
+    padding: var(--t-space-48);
+    border-radius: var(--t-br-large);
+  }
+  .auth-forgot-password-input {
+    max-width: 85%;
+  }
+  .auth-forgot-password-footer-link {
+    font-size: var(--t-fs-small);
+  }
 }
 </style>
