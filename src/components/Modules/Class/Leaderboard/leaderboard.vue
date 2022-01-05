a945e56cf2b90db1fb5c18b25ec9db6786207ef9
Muaaz Seedat <muaaztest@gmail.com>
2022-01-05 10:52:49 +0000

    chore: configure css

--- src/components/Modules/Class/Leaderboard/leaderboard.vue
+++ src/components/Modules/Class/Leaderboard/leaderboard.vue
@@ -12,3 +12,47 @@ export default {
   name: "LeaderboardClasses",
 };
 </script>
+<style scoped>
+.box-container {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 100px 0 var(--t-space-50) 0;
+  gap: 10px;
+}
+.box-content {
+  display: grid;
+  width: 70%;
+  background-color: white;
+  border-radius: var(--t-br-medium);
+  padding: var(--t-space-20);
+  gap: 10px;
+}
+.back {
+  text-decoration: none;
+}
+@media (max-width: 599px) {
+  .box-content {
+    width: 100%;
+  }
+  .box-container {
+    padding: 110px 15px var(--t-space-50) 10px;
+  }
+}
+@media (min-width: 600px) {
+  .back-default {
+    position: absolute;
+    left: 160px;
+  }
+}
+@media (min-width: 900px) {
+}
+@media (min-width: 1200px) {
+  .back-default {
+    position: absolute;
+    left: 160px;
+  }
+}
+</style>
