1feda63c518fd185322a9035a01ad2135ed0de99
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-01-10 15:05:50 +0500

    feat: topic card ui made responsive - cards - submodules

--- src/components/SubModules/Cards/Topic/Topic.vue
+++ src/components/SubModules/Cards/Topic/Topic.vue
@@ -38,8 +38,26 @@ export default {
   align-items: center;
   background-color: var(--t-white);
   width: fit-content;
-  border-radius: var(--t-br-large);
-  padding: var(--t-space-24) var(--t-space-16);
-  gap: var(--t-space-16);
+}
+
+/* Responsive variants */
+@media (max-width: 599px) {
+  .talkie-topic-card-wrapper {
+    border-radius: var(--t-br-small);
+    padding: var(--t-space-16) var(--t-space-12);
+    gap: var(--t-space-12);
+  }
+}
+@media (min-width: 600px) {
+  .talkie-topic-card-wrapper {
+    border-radius: var(--t-br-medium);
+    padding: var(--t-space-24) var(--t-space-16);
+    gap: var(--t-space-16);
+  }
+}
+@media (min-width: 900px) {
+  .talkie-topic-card-wrapper {
+    border-radius: var(--t-br-large);
+  }
 }
 </style>
