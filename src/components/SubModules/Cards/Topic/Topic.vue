96d18d65c5c7f04add92f422fa5f4e365a5cd1db
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-01-10 15:04:59 +0500

    feat: topic card - cards -submodules

---
+++ src/components/SubModules/Cards/Topic/Topic.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="['talkie-topic-card-wrapper', customClass.toString()]">
+    <talkie-check-box :defaultChecked="topicSelected" :label="topicName" />
+  </div>
+</template>
+
+<script>
+import { TalkieCheckBox } from "@/components/UICore";
+
+export default {
+  name: "TopicCard",
+  data() {
+    return {};
+  },
+  components: {
+    TalkieCheckBox,
+  },
+  props: {
+    topicName: {
+      type: String,
+    },
+    topicSelected: {
+      type: Boolean,
+      default: false,
+    },
+    customClass: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style>
+.talkie-topic-card-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--t-white);
+  width: fit-content;
+  border-radius: var(--t-br-large);
+  padding: var(--t-space-24) var(--t-space-16);
+  gap: var(--t-space-16);
+}
+</style>
