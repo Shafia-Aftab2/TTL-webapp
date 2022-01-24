e9193104da8055e3391f0c4ff90c5951448cc539
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-01-24 14:33:10 +0500

    refactor: leaderboard ui

--- src/components/Modules/Class/Leaderboard/leaderboard.vue
+++ src/components/Modules/Class/Leaderboard/leaderboard.vue
@@ -1,204 +1,213 @@
 <template>
-  <div class="box-container">
-    <div>
-      <div class="back-default">
-        <a><span>&#8592; </span>back</a>
+  <!-- Content Wrapper -->
+  <div class="class-leader-wrapper" v-if="!computedPageLoading">
+    <div class="class-leader-header-wrapper">
+      <div class="class-leader-header-details-wrapper">
+        <h2 class="h2" v-if="classDetails.name">
+          <a
+            :href="computedClassHomeLink"
+            class="class-leader-header-details-class-name-link"
+          >
+            {{ classDetails.name }}
+          </a>
+        </h2>
       </div>
     </div>
-    <div class="box-content">
-      <div class="box-header">
-        <div class="overview">
-          <div class="ellipse">
-            <img
-              src="../../../../assets/images/Ellipse_55.png"
-              alt="Image here"
-            />
-          </div>
-          <div class="student-box">
-            <div class="student_name">
-              <p>Harris D</p>
-            </div>
-            <div class="points">
-              <p>1432</p>
-            </div>
-          </div>
-          <div class="trophy">
-            <img src="../../../../assets/images/trophy.png" alt="Image here" />
-          </div>
-        </div>
-        <div class="box-1">
-          <div class="img">
-            <img
-              src="../../../../assets/images/Ellipse_56.png"
-              alt="Image here"
-            />
-          </div>
-          <div class="name">
-            <p>Harris D</p>
-          </div>
-          <div class="tt">
-            <p>1432 TTs</p>
-          </div>
-        </div>
-        <div class="box-2">
-          <img src="../../../../assets/images/icon2.png" alt="Image here" />
-          <div class="name">
-            <p>Jamie S</p>
-          </div>
-          <div class="tt">
-            <p>1211 TTs</p>
-          </div>
-        </div>
-        <div class="box-3 bar-outline">
-          <img src="../../../../assets/images/icon3.png" alt="Image here" />
-          <div class="name">
-            <p>Mia P</p>
-          </div>
-          <div class="tt">
-            <p>1204 TTs</p>
-          </div>
-        </div>
-        <div class="box-4 bar-outline">
-          <img src="../../../../assets/images/icon4.png" alt="Image here" />
-          <div class="name">
-            <p>Alice G</p>
-          </div>
-          <div class="tt">
-            <p>989 TTs</p>
-          </div>
-        </div>
-      </div>
+    <div class="class-leader-content-wrapper">
+      <talkie-student-card
+        v-for="_student in classStudents"
+        :key="_student"
+        :mode="'points'"
+        :customClass="'class-leader-content-card'"
+        :studentAvatar="_student.image"
+        :studentName="_student.name"
+        :studentPoints="8413"
+      />
     </div>
   </div>
+
+  <!-- Load Wrapper -->
+  <div class="class-manage-load-wrapper" v-if="computedPageLoading">
+    <talkie-loader :size="'large'" />
+  </div>
 </template>
+
 <script>
+import { TalkieLoader } from "@/components/UICore";
+import { TalkieStudentCard } from "@/components/SubModules/Cards";
+
 export default {
-  name: "LeaderboardClasses",
+  name: "ClassLeaderboard",
+  components: { TalkieLoader, TalkieStudentCard },
+  data() {
+    return {
+      classId: null,
+      pageLoading: false,
+      classDetails: {
+        name: "Spanish 10A",
+        id: 8654123661,
+        students: [],
+      },
+    };
+  },
+  computed: {
+    computedClassHomeLink() {
+      return `${window.location.origin}/classes/${this.classId}`;
+    },
+    computedPageLoading() {
+      return this.pageLoading;
+    },
+  },
+  async created() {
+    // class id from params
+    const classId = this.$route.params.classId;
+    this.classId = classId;
+  },
 };
 </script>
-<style scoped>
-.overview {
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  padding: 20px;
-}
-.box-header {
-  width: 370%;
-}
-.bar-outline {
-  background-color: var(--t-white);
-  padding: var(--t-space-16);
-  border-radius: var(--t-space-10);
-  border: 2px solid var(--t-gray-home);
-}
-.box-1 {
-  margin: 20px;
-  position: relative;
-  border-radius: 16px;
-  background: #f9e5d6;
-  padding: 10px;
-  display: flex;
-  align-items: left;
-  width: 100%;
-}
-.box-2 {
-  margin: 20px;
-  position: relative;
-  border-radius: 16px;
-  background: #fbf6de;
-  padding: 10px;
-  display: flex;
-  width: 100%;
-}
-.box-3 {
-  margin: 20px;
-  position: relative;
-  border-radius: 16px;
-  padding: 10px;
-  display: flex;
-  width: 100%;
-}
-.box-4 {
-  margin: 20px;
-  position: relative;
-  border-radius: 16px;
-  padding: 10px;
-  display: flex;
-  width: 100%;
-}
-.box-container {
-  position: relative;
+
+<style>
+.class-leader-wrapper {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  padding: 100px 0 var(--t-space-50) 0;
-  gap: 10px;
+  margin: auto;
+  margin-top: var(--t-space-36);
+  margin-bottom: var(--t-space-36);
+  background-color: var(--t-white);
 }
-.box-content {
-  display: grid;
-  width: 100%;
-  background-color: white;
-  border-radius: var(--t-br-medium);
-  padding: var(--t-space-70);
-  gap: 10px;
-  margin-top: 20px;
+.class-leader-header-wrapper {
   display: flex;
-}
-.back {
-  text-decoration: none;
-}
-.ellipse {
-  margin: 20px;
-}
-.trophy {
-  margin: 20px;
-}
-.student-box {
-  display: grid;
-  width: 100%;
-  background-color: white;
-  border-radius: var(--t-br-medium);
-  align-items: center;
   justify-content: center;
+  align-items: center;
+}
+.class-leader-header-details-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+}
+.class-leader-header-details-class-name-link,
+.class-leader-header-details-class-name-link:visited {
+  text-decoration: none;
+  color: var(--t-black);
+}
+.class-leader-header-details-update-form-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.class-leader-header-details-tab-options-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.class-leader-header-details-manage-options-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: flex-start;
+}
+.class-leader-content-wrapper {
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+}
+.class-leader-content-card {
+  border: var(--t-space-1) solid var(--t-gray-75);
+}
+.class-leader-modal-invite-students {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: var(--t-space-30);
+}
+.class-leader-modal-invite-students-header-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: var(--t-space-5);
   text-align: center;
 }
-.name {
-  flex: 3;
-  padding: 10px;
+.class-leader-modal-invite-students-input-wrapper,
+.class-leader-modal-invite-students-input-wrapper > div {
+  min-width: 80% !important;
 }
-.tt {
-  padding: 10px;
-  font-weight: bold;
+.class-leader-load-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  align-items: center;
+  margin-top: var(--t-space-36);
+  margin-bottom: var(--t-space-36);
 }
-.student_name {
-  font-size: 30px;
-  font-weight: bold;
-}
-.points {
-  font-size: 30px;
-  font-weight: bold;
-}
+
+/* Responsive variants */
 @media (max-width: 599px) {
-  .box-content {
-    width: 100%;
+  .class-leader-wrapper {
+    max-width: 100%;
+    padding: var(--t-space-32);
+    border-radius: var(--t-br-medium);
+    gap: calc(var(--t-space-24) * 2);
   }
-  .box-container {
-    padding: 110px 15px var(--t-space-50) 10px;
+  .class-leader-header-details-wrapper {
+    gap: var(--t-space-8);
+  }
+  .class-leader-header-details-update-form-wrapper {
+    gap: var(--t-space-5);
+  }
+  .class-leader-header-details-tab-options-wrapper {
+    gap: var(--t-space-8);
+  }
+  .class-leader-header-details-manage-options-wrapper {
+    gap: var(--t-space-3);
+  }
+  .class-leader-content-wrapper {
+    gap: var(--t-space-8);
   }
 }
 @media (min-width: 600px) {
-  .back-default {
-    position: absolute;
-    left: 160px;
+  .class-leader-wrapper {
+    max-width: 80%;
+    padding: var(--t-space-48);
+    border-radius: var(--t-br-medium);
+    gap: calc(var(--t-space-16) * 2);
+  }
+  .class-leader-header-details-wrapper {
+    gap: var(--t-space-8);
+  }
+  .class-leader-header-details-update-form-wrapper {
+    gap: var(--t-space-10);
+  }
+  .class-leader-header-details-tab-options-wrapper {
+    gap: var(--t-space-8);
+  }
+  .class-leader-header-details-manage-options-wrapper {
+    gap: var(--t-space-5);
+  }
+  .class-leader-content-wrapper {
+    gap: var(--t-space-8);
   }
 }
-@media (min-width: 900px) {
-}
 @media (min-width: 1200px) {
-  .back-default {
-    position: absolute;
-    left: 5px;
+  .class-leader-wrapper {
+    max-width: 90%;
+    padding: var(--t-space-48);
+    border-radius: var(--t-br-large);
+    gap: calc(var(--t-space-36) * 2);
+  }
+  .class-leader-header-details-wrapper {
+    gap: var(--t-space-16);
+  }
+  .class-leader-header-details-update-form-wrapper {
+    gap: var(--t-space-12);
+  }
+  .class-leader-header-details-tab-options-wrapper {
+    gap: var(--t-space-12);
+  }
+  .class-leader-content-wrapper {
+    gap: var(--t-space-12);
   }
 }
 </style>
