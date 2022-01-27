61d412fdeaf07363a8ee2949b5d78ca8801a40f5
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-01-27 13:58:50 +0500

    chore: added create caption task validation - valiations

--- src/utils/validations/task.validation.js
+++ src/utils/validations/task.validation.js
@@ -16,4 +16,16 @@ const updateQandATopicSchema = yup.object({
   questionText: yup.string().optional(),
 });
 
-export { createQandATopicSchema, updateQandATopicSchema };
+const createCaptionThisTopicSchema = yup.object({
+  topic: yup.string().required("Topic is required."),
+  title: yup.string().required("Title is required."),
+  type: yup.string().default(taskTypes.CAPTION_THIS).required(),
+  questionText: yup.string().optional(),
+  captionImage: yup.mixed().required("Caption image is required."),
+});
+
+export {
+  createQandATopicSchema,
+  updateQandATopicSchema,
+  createCaptionThisTopicSchema,
+};
