345bb0e784e237b1527a08360e3d7642606001f3
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-03-02 20:23:42 +0500

    chore: added sidebar mutation mixin utility

---
+++ src/utils/mixins/handleSidebarMutation.js
@@ -0,0 +1,27 @@
+export default {
+  methods: {
+    handleStoreMutation(key, value) {
+      this.$store.state[key] = value;
+    },
+    handleSidebarMutation(data) {
+      const sidebar = this.$store.state.sidebar;
+      const updatedData = {
+        hasBackLink: data.hasOwnProperty("hasBackLink")
+          ? data.hasBackLink
+          : sidebar.hasBackLink,
+        items: data.hasOwnProperty("items") ? data.items : sidebar.items,
+        checkboxes: data.hasOwnProperty("checkboxes")
+          ? data.checkboxes
+          : sidebar.checkboxes,
+        buttons: data.hasOwnProperty("buttons")
+          ? data.buttons
+          : sidebar.buttons,
+      };
+
+      this.handleStoreMutation(
+        "sidebar",
+        Object.assign({}, { ...updatedData })
+      );
+    },
+  },
+};
