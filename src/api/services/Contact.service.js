8cae945651430f885391da9097b9a67997050b02
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-06-02 02:30:11 +0500

    chore: contact us api end point updated

--- src/api/services/Contact.service.js
+++ src/api/services/Contact.service.js
@@ -2,6 +2,6 @@ import HTTPClient from "../HTTPClient";
 
 export default class AuthRoutes {
   static async SendMessage(payload) {
-    return HTTPClient.post(`/contact`, payload);
+    return HTTPClient.post(`/contact-us`, payload);
   }
 }
