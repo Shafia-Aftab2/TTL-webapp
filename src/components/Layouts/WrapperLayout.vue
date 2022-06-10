e12ce602c309d1dcf28eae8bb9d3a23dd7067cf6
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-06-10 15:55:19 +0500

    fix: layout shift due to trail bar on wrapper layout

--- src/components/Layouts/WrapperLayout.vue
+++ src/components/Layouts/WrapperLayout.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="talkie-layout-wrapper">
+  <div
+    class="talkie-layout-wrapper"
+    :class="[
+      'talkie-navbar-wrapper',
+      computedSubscription?.remainingTrialDays > 0 &&
+        computedSubscription?.remainingTrialDays < 15 &&
+        !computedSubscription?.hideTrialBar &&
+        'talkie-wrapper-trial-pad',
+    ]"
+  >
     <router-view />
   </div>
 </template>
@@ -7,6 +16,11 @@
 <script>
 export default {
   name: "WrapperLayout",
+  methods: {
+    computedSubscription() {
+      return this.$store.state.subscription;
+    },
+  },
 };
 </script>
 
@@ -21,11 +35,17 @@ export default {
     max-width: var(--t-grid-small);
     padding-top: var(--t-space-50);
   }
+  .talkie-wrapper-trial-pad {
+    margin-top: 25px; /* TODO: get from variables */
+  }
 }
 @media (min-width: 600px) {
   .talkie-layout-wrapper {
     max-width: var(--t-grid-large);
     padding-top: var(--t-space-58);
+  }
+  .talkie-wrapper-trial-pad {
+    margin-top: var(--t-space-32);
   }
 }
 @media (min-width: 1200px) {
