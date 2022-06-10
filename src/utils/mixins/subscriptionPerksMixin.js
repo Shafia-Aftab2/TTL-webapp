42a69217eb758d846bb229eccfdbb57f09d52b97
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-06-10 17:27:22 +0500

    chore(temp): block user from creating more than 8 classes if subscription plan does not allow

--- src/utils/mixins/subscriptionPerksMixin.js
+++ src/utils/mixins/subscriptionPerksMixin.js
@@ -1,4 +1,4 @@
-import { SubscriptionService } from "@/api/services";
+import { SubscriptionService, ClassService } from "@/api/services";
 import { pricingPlans } from "../constants";
 
 export default {
@@ -8,7 +8,13 @@ export default {
         pricingPlans.planNames.BUDDING_SPEAKERS,
         pricingPlans.planNames.LONG_HAUL,
       ]?.map((x) => x?.toLowerCase()?.split("-")?.join(" ")),
+      plansAllowingUnlimitedClasses: [pricingPlans.planNames.LONG_HAUL]?.map(
+        (x) => x?.toLowerCase()?.split("-")?.join(" ")
+      ),
       canDownloadContent: false,
+      canCreateUnlimitedClasses: false,
+      maxClassCount: 8,
+      currentClassCount: 0 /*** NOTE: this line is to be removed, and related data has to come from api ***/,
     };
   },
   async created() {
@@ -20,15 +26,32 @@ export default {
       ?.split("-")
       ?.join(" ");
 
+    // check if user can download content
     if (planName && this.plansAllowingDownloads?.includes(planName)) {
       this.canDownloadContent = true;
     }
+
+    // check if user can create unlimited classes
+    if (planName && this.plansAllowingUnlimitedClasses?.includes(planName)) {
+      this.canCreateUnlimitedClasses = true;
+    }
+
+    /*** NOTE: this block is to be removed, and related data has to come from api ***/
+    // get the number of classes user has created
+    const currentClassCount = ((await this.getMyClasses()) || Array.length(8))
+      ?.length;
+    this.currentClassCount = currentClassCount;
   },
   methods: {
     async getMySubscription() {
       const response = await SubscriptionService.GetMySubscription().catch(
         () => null
       );
+
+      return response?.data || null;
+    },
+    async getMyClasses() {
+      const response = await ClassService.GetMyClasses().catch(() => null);
 
       return response?.data || null;
     },
